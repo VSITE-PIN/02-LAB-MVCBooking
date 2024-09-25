9ac0f05627cccefd8010822905a7b59d64191404
Paolo Stabile <pastabil@vsite.hr>
2024-09-25 16:41:14 +0200

    igor_breskiAdd project files.

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+
+namespace MVCBooking.Controllers
+{
+
+    public class BookingController : Controller
+    {
+        private static int autoId = 1;
+        private static List<Booking> bookings = new List<Booking>();
+
+        public IActionResult Index()
+        {      
+            return View(bookings);
+        }
+        public IActionResult Create()
+        {
+            Booking booking = new Booking();
+            booking.Id = autoId++;
+            return View(booking);
+        }
+
+
+        public IActionResult CreateBooking(Booking bookingViewModel)
+        {
+            
+            bookings.Add(bookingViewModel);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
+
---
+++ MVCBooking/Models/Booking.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCBooking.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int RoomNumber { get; set; }
+        public string GuestName { get; set; }
+    }
+}
