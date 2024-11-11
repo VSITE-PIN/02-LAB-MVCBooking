ad65bb24ff1bf6c61b22d118e975df013fc1511a
Leon Berisha <leberisha@vsite.hr>
2024-11-11 21:03:47 +0100

    Vjezba 2

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCapp2.Models;
+
+namespace MVCapp2.Controllers
+{
+    public class BookingController : Controller
+    {
+        private static List<HotelBooking> bookings = new List<HotelBooking>();
+        public IActionResult Index()
+        {
+            return View(bookings);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateBooking(HotelBooking booking)
+        {
+            booking.Id = bookings.Count + 1;
+            bookings.Add(booking);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVCBooking/Models/HotelBooking.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace MVCapp2.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+        public string GuestName { get; set; }
+        public int RoomNumber { get; set; }
+    }
+
+    } 
