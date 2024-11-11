eeaa74e7ec82ab0bcc220f94fe77c4ce885a84c3
letomic3 <letomic@vsite.hr>
2024-11-12 00:29:02 +0100

    Lab 2

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+using System.Collections.Generic;
+
+namespace MVCBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        // Simulirana lista bookinga za demonstraciju
+        private static List<HotelBooking> bookings = new List<HotelBooking>();
+
+        // Index metoda za prikaz svih bookinga
+        public IActionResult Index()
+        {
+            return View(bookings);
+        }
+
+        // Create metoda za prikaz Create view-a
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // CreateBooking metoda za dodavanje novog bookinga
+        [HttpPost]
+        public IActionResult CreateBooking(HotelBooking booking)
+        {
+            if (ModelState.IsValid)
+            {
+                bookings.Add(booking);
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+    }
+}
+
