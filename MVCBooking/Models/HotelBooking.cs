03316fbf3cd7d725847cbfec95aab3e4ab58b643
KriZa96 <kriza9605@gmail.com>
2024-11-11 21:01:32 +0100

    Small fix to run github actions

--- MVCBooking/Models/HotelBooking.cs
+++ MVCBooking/Models/HotelBooking.cs
@@ -2,6 +2,7 @@
 {
     public class HotelBooking
     {
+        // Mali change za action
         public int Id { get; set; }
         public string GuestName { get; set; }
         public int RoomNumber { get; set; }
