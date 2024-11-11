fd2773c0e47fb5aeef9cf646f951dbb090d23c6a
veronikamataija1 <vemataija@vsite.hr>
2024-11-11 21:17:11 +0100

    Labos 2

---
+++ MVCBooking/Controllers/BookingContoller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+
+namespace MVCBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        private static List<HotelBooking> bookings = new List<HotelBooking>();
+
+        public IActionResult Index()
+        {
+            return View(bookings);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBooking(HotelBooking booking)
+        {
+            bookings.Add(booking);
+            return RedirectToAction("Index");
+        }
+    }
+
+}
