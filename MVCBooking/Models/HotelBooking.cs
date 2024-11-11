e7f0759d945eebeae4054a225e27dfc78779e021
Mia Fidermuc Vekić <mifiderm@vsite.hr>
2024-11-11 20:19:46 +0100

    lab2

---
+++ MVCBooking/Models/HotelBooking.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace MVCapp2.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+        public string GuestName { get; set; }
+        public int RoomNumber { get; set; }
+    }
+
+}
+
