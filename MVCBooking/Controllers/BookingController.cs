00edf8293e577989f0d1a982a4bb092e859b6008
Tomislav Prstec <toprstec@vsite.hr>
2024-11-11 22:56:06 +0100

    lab02 solved

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+
+namespace MVCBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        private static List<HotelBooking> bookings = new List<HotelBooking>();
+        public IActionResult Index()
+        {
+            return View(bookings);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new HotelBooking());
+        }
+
+        public IActionResult CreateBooking(HotelBooking newBooking)
+        {
+            newBooking.Id = bookings.Count;
+            bookings.Add(newBooking);
+            return RedirectToAction("Index");
+        }
+    }
+}
