faf7c3e633b211745c2639b00d2f55535731db5c
6ante9 <anpalic@vsite.hr>
2024-11-12 21:11:37 +0100

    Labos 2

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+
+namespace MVCBooking.Controllers
+{
+
+    public class BookingController : Controller
+    {
+        private static List<Booking> HotelBookings = new List<Booking>();
+        private static int nextId = 1;
+        public IActionResult Index()
+        {
+            return View(HotelBookings);
+        }
+        public IActionResult Create()
+        {
+            Booking hotelBooking = new Booking();
+            return View(hotelBooking);
+        }
+        public IActionResult CreateBooking(Booking hotelBookingViewModel)
+        {
+            hotelBookingViewModel.Id = nextId++;
+            HotelBookings.Add(hotelBookingViewModel);
+            return RedirectToAction("Index");
+        }
+    }
+}
