de8e20517198e3674ec4f7301bac77717e16c186
Filip Maretić <fimareti@vsite.hr>
2024-11-12 20:09:10 +0100

    Za potpis

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+using System.Collections.Generic;
+
+namespace MVCBooking.Controllers
+{
+	public class BookingController : Controller
+	{
+		
+		private static List<HotelBooking> bookings = new List<HotelBooking>();
+
+		
+		public IActionResult Index()
+		{
+			return View(bookings);
+		}
+
+		
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		
+		[HttpPost]
+		public IActionResult CreateBooking(HotelBooking booking)
+		{
+			
+			booking.Id = bookings.Count + 1;
+			bookings.Add(booking);
+			return RedirectToAction("Index");
+		}
+	}
+}
