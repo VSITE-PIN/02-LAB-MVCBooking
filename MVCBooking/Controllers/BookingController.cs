4663dda3e2217a91b659d448f7500a38e3151d21
Dorijan Ivanković <doivanko@vsite.hr>
2024-11-14 20:43:11 +0100

    labos 2

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+
+namespace MVCBooking.Controllers
+{
+	public class BookingController : Controller
+	{
+		private static List<HotelBooking> booking = new List<HotelBooking>();
+		public IActionResult Index()
+		{
+			return View(booking);
+		}
+		public IActionResult Create()
+		{
+			HotelBooking booking = new HotelBooking();
+			return View(booking);
+		}
+		public IActionResult CreateBooking(HotelBooking hotelViewModel)
+		{
+			hotelViewModel.Id = booking.Count() + 1;
+			booking.Add(hotelViewModel);
+			return RedirectToAction("Index");
+		}
+	}
+}
