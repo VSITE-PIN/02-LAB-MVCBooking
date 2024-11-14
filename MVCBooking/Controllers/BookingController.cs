07abf89bfebb35dba2c51f36c310cde017c1ba21
Lovro Progar <loprogar@vsite.hr>
2024-11-14 20:33:39 +0100

    rijesen 2 lab

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+
+namespace MVCBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        private static List<HotelBooking> bookings = new List<HotelBooking>();
+        public IActionResult Index()
+        {
+            return View(bookings);
+        }
+        public IActionResult Create()
+        {
+            HotelBooking hotel = new HotelBooking();
+            return View(hotel);
+        }
+        public IActionResult CreateBooking(HotelBooking hotelModel)
+        {
+            bookings.Add(hotelModel);
+            return RedirectToAction("Index");
+        }
+    }
+}
