ac4710fbf148ebd12308d4a018f8703c79845b35
Lara Mihelin <lamihelin@vsite.hr>
2024-11-14 19:27:16 +0100

    LAB02

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+
+namespace MVCBooking.Controllers
+{
+
+	public class BookingController : Controller
+	{
+
+		private static List<HotelBooking> bookings = new List<HotelBooking>();
+		public IActionResult Index()
+		{
+			/** HotelBooking booking = new HotelBooking();
+			 booking.Id = 1;
+			 booking.RoomNumber = 1;
+			 booking.GuestName = "Sylvia Plath";**/
+			return View(bookings);
+		}
+
+		public IActionResult Create()
+		{
+			HotelBooking hotelBooking = new HotelBooking();
+			return View(hotelBooking);
+		}
+
+		public IActionResult CreateBooking(HotelBooking hotelViewModel)
+		{
+			bookings.Add(hotelViewModel);
+			return RedirectToAction("Index");
+		}
+
+	}
+}
+
---
+++ MVCBooking/Models/HotelBooking.cs
@@ -0,0 +1,13 @@
+﻿namespace MVCBooking.Models
+{
+	public class HotelBooking
+	{
+		public int Id { get; set; }
+		public String GuestName { get; set; }
+
+		public int RoomNumber { get; set; }
+
+
+
+	}
+}
