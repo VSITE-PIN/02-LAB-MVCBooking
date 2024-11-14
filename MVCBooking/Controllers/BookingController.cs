74738b9f7273982d8e1bbeb04af3dcb691f8652f
Davor Petrlić <dapetrli@vsite.hr>
2024-11-14 20:48:06 +0100

    Vježba 2

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+
+namespace MVCBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Bookings);
+        }
+        private static List<HotelBooking> Bookings = new List<HotelBooking>();
+        public IActionResult Create()
+        {
+            HotelBooking booking = new HotelBooking();
+            return View(booking);
+        }
+        public IActionResult CreateBooking(HotelBooking bookingViewModel)
+        {
+            Bookings.Add(bookingViewModel);
+            return RedirectToAction("Index");
+        }
+    }
+}
