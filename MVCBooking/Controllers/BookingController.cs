61c2449b5bfe6817cb1f8e4f7aa2e3903a12f8d5
Franko Junakovic <franko.junakovic@vsite.hr>
2024-11-14 19:43:38 +0100

    Resolved LAB2

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+
+namespace MVCBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        private static List<HotelBooking> hotelBookings = new List<HotelBooking>();
+        private static int _id = 1;
+
+        public IActionResult Index()
+        {
+            return View(hotelBookings);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new HotelBooking());
+        }
+
+        public IActionResult CreateBooking(HotelBooking newBooking)
+        {
+            if (newBooking != null)
+            {
+                newBooking.Id = _id++;
+                hotelBookings.Add(newBooking);
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Create");
+        }
+    }
+}
