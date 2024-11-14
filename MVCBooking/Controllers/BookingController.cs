9efc7daad48b4a2d1eb12bbeb71b19058147189e
IvanTrosic <ivtrosic@vsite.hr>
2024-11-14 20:40:49 +0100

    commit lab2

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+using System.Collections.Generic;
+using MVCBooking.Models;
+
+namespace YourNamespace.Controllers
+{
+    public class BookingController : Controller
+    {
+        // Staticka lista za čuvanje booking podataka u memoriji
+        private static List<HotelBooking> bookings = new List<HotelBooking>();
+
+        // Prikazivanje svih booking-a
+        public IActionResult Index()
+        {
+            return View(bookings); // Vraća sve booking-e u Index view
+        }
+
+        // Prikazivanje forme za kreiranje novog booking-a
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Dodavanje novog booking-a
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateBooking(HotelBooking newBooking)
+        {
+            if (ModelState.IsValid)
+            {
+                // Dodavanje novog booking-a u memorijsku listu
+                bookings.Add(newBooking);
+
+                // Nakon dodavanja, preusmeravamo na Index gde će se videti svi booking-i
+                return RedirectToAction("Index");
+            }
+
+            // Ako validacija ne uspe, vraćamo korisnika na formu s postojećim podacima
+            return View("Create", newBooking);
+        }
+    }
+}
