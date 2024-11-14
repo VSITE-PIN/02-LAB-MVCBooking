9a5e6c6be7c527d241b33f4436027b92ff7a2ef0
Damir Magdić <damir.magdic@vsite.hr>
2024-11-14 20:28:55 +0100

    HotelBooking - finished

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+using System.Linq;
+
+namespace MVCBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        private static List<HotelBooking> bookings = new List<HotelBooking>();
+        public IActionResult Index()
+        {
+            return View(bookings);
+        }
+
+        public IActionResult Create()
+        {
+            HotelBooking booking = new HotelBooking();
+            return View(booking);
+        }
+        public IActionResult CreateBooking(HotelBooking bookingViewModel)
+        {
+            bookings.Add(bookingViewModel);
+            return RedirectToAction("Index");
+        }
+    }
+}
