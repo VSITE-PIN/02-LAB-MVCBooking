06f8edad7dc1c4bdb8bb95b29f95e8137447bf31
Kristijan Popović <krpopovi@vsite.hr>
2024-11-14 20:18:08 +0100

    Ajdudn

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+
+namespace MVCBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        private static List<HotelBooking> hotel = new List<HotelBooking>();
+        public IActionResult Index()
+        {
+            return View(hotel);
+        }
+        public IActionResult Create()
+        {
+            HotelBooking hotel = new HotelBooking();
+            return View(hotel);
+        }
+        public IActionResult CreateBooking(HotelBooking hotelViewModel)
+        {
+            hotel.Add(hotelViewModel);
+            return RedirectToAction("Index");
+        }
+    }
+}
